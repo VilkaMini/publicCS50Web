cc0409917f7d21c592ec213434c0163f51393391
Vilius <alaunisvilius@gmail.com>
2019-10-10 18:47:31 +0300

    Basic Htmls

--- project1/application.py
+++ project1/application.py
@@ -1,4 +1,7 @@
 import os
+# import requests
+# res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "WSPQYHxOwJq5n8j4TsQQA", "isbns": "9781632168146"})
+# print(res.json())
 
 from flask import Flask, render_template, session
 from flask_session import Session
@@ -24,3 +27,11 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def Login():
     return render_template("login.html")
+
+@app.route("/register")
+def Register():
+    return render_template("register.html")
+
+@app.route("/search")
+def Search():
+    return render_template("Search.html")
