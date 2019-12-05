23f1daec7283502c9e0e86f84520aa9768e037a6
Vilius <alaunisvilius@gmail.com>
2019-12-05 18:52:16 +0200

    Start

--- project2/application.py
+++ project2/application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -10,4 +10,11 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+if __name__ == "__main__":
+    socketio.run(app)
