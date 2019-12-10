6b40ba9b3c4423563c7a63e8f71819040d13b278
Vilius <alaunisvilius@gmail.com>
2019-12-10 18:54:48 +0200

    Message server received

--- project2/application.py
+++ project2/application.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -12,9 +12,27 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
+@socketio.on('con')
+def handle_con(con):
+    print(con['data'])
+
+@socketio.on('username')
+def handle_message(username):
+    print('username : ' + username['data'])
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    emit(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    emit(username + ' has left the room.', room=room)
 
 if __name__ == "__main__":
     socketio.run(app)
