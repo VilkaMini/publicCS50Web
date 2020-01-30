af7d16769fa5956a60141d6454ed142aee3ecdd0
Vilius <alaunisvilius@gmail.com>
2020-01-30 18:53:12 +0200

    Session

--- project3/orders/views.py
+++ project3/orders/views.py
@@ -10,7 +10,11 @@ from .models import Toppings, Sub, Pasta, Salads, Dinner, Size, Kind, Pizza
 def index(request):
     if not request.user.is_authenticated:
         return render(request, "orders/login.html")
+    if not request.session:
+        request.session["saved"] = []
+        request.session["total"] = [0]
     context = menuHelper()
+    context["ordered"] = request.session.get("saved", [])
     return render(request, "orders/index.html", context)
 
 def register(request):
@@ -59,35 +63,68 @@ def pizza(request):
     topping2 = request.POST["topping2"]
     topping3 = request.POST["topping3"]
     price = Pizza.objects.all().filter(name=name, kind__kind=kind, size__size=size)
+    price = price.values_list("price", flat=True)[0]
     context = menuHelper()
-    print(price.values_list("price", flat=True)[0])
-    print(kind, size, name, topping1, topping2, topping3)
+    total = request.session.get("total", [])
+    total = total[0] + price
+    request.session["total"] = total
+    print(total)
+    saved_list = request.session.get("saved", [])
+    saved_list.append([kind, size, name, price])
+    request.session["saved"] = saved_list
+    context["ordered"] = request.session.get("saved", [])
     return render(request, "orders/index.html", context)
 
 def sub(request):
     size = request.POST["subSize"]
     name = request.POST["subName"]
+    price = Sub.objects.all().filter(sub=name)
+    if size == "Small":
+        price = price.values_list("smallPrice", flat=True)[0]
+    else:
+        price = price.values_list("largePrice", flat=True)[0]
     context = menuHelper()
-    print(size, name)
+    saved_list = request.session.get("saved", [])
+    saved_list.append([size, name, price])
+    request.session["saved"] = saved_list
+    context["ordered"] = request.session.get("saved", [])
     return render(request, "orders/index.html", context)
 
 def pasta(request):
     name = request.POST["pastaName"]
+    price = Pasta.objects.all().filter(pasta=name)
+    price = price.values_list("price", flat=True)[0]
     context = menuHelper()
-    print(name)
+    saved_list = request.session.get("saved", [])
+    saved_list.append([name, price])
+    request.session["saved"] = saved_list
+    context["ordered"] = request.session.get("saved", [])
     return render(request, "orders/index.html", context)
 
 def salad(request):
     name = request.POST["saladName"]
+    price = Salads.objects.all().filter(salad=name)
+    price = price.values_list("price", flat=True)[0]
     context = menuHelper()
-    print(name)
+    saved_list = request.session.get("saved", [])
+    saved_list.append([name, price])
+    request.session["saved"] = saved_list
+    context["ordered"] = request.session.get("saved", [])
     return render(request, "orders/index.html", context)
 
 def dinner(request):
     size = request.POST["dinnerSize"]
     name = request.POST["dinnerName"]
+    price = Dinner.objects.all().filter(plate=name)
+    if size == "Small":
+        price = price.values_list("smallPrice", flat=True)[0]
+    else:
+        price = price.values_list("largePrice", flat=True)[0]
     context = menuHelper()
-    print(size, name)
+    saved_list = request.session.get("saved", [])
+    saved_list.append([size, name, price])
+    request.session["saved"] = saved_list
+    context["ordered"] = request.session.get("saved", [])
     return render(request, "orders/index.html", context)
 
 
